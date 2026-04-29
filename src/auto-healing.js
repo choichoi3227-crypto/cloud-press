4587ec10a5842ddeb1bf948b18e9869a66b5e31a
choichoi3227-crypto <choichoi3227@gmail.com>
2026-04-29 14:51:33 +0900

    Create auto-healing.js

---
+++ src/auto-healing.js
@@ -0,0 +1,5 @@
+export class AutoHealing {
+  constructor(env, storageRouter) { /* ... 기존 로직 ... */ }
+  async monitorAndHeal() { /* ... 기존 로직 ... */ }
+  async healBucket(failedIdx) { /* ... 기존 로직 ... */ }
+}

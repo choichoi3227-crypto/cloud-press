75287f1308bdcb66c618ac328cb29e4d3372bee7
choichoi3227-crypto <choichoi3227@gmail.com>
2026-05-10 13:28:17 +0900

    Update [[path]].js

--- functions/api/notify/[[path]].js
+++ functions/api/notify/[[path]].js
@@ -1,4 +1,10 @@
 // functions/api/notify/[[path]].js
-// /api/notify/* 경로를 notify.js 핸들러로 위임
-import { onRequestGet, onRequestPost, onRequestOptions } from "../notify.js";
+// /api/notify/* 경로를 notify.js로 위임
+
+import {
+  onRequestGet,
+  onRequestPost,
+  onRequestOptions,
+} from "../notify.js";
+
 export { onRequestGet, onRequestPost, onRequestOptions };
